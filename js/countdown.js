7862e804441b19b6e9094730452602b34dde9d7a
kikutaro <kikutaro526@yahoo.co.jp>
2018-07-06 22:37:54 +0900

    乃木坂46バスラ2日目のカウントダウン開始

--- js/countdown.js
+++ js/countdown.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("July 6, 2018 18:00:00").getTime();
+var countDownDate = new Date("July 7, 2018 17:00:00").getTime();
 
 var x = setInterval(function() {
 
@@ -9,7 +9,7 @@ var x = setInterval(function() {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  document.getElementById("countdown").innerHTML = "乃木坂46 6th YEAR BIRTHDAY LIVE まで<br>" + days + "日 " + hours + "時間 "
+  document.getElementById("countdown").innerHTML = "乃木坂46 6th YEAR BIRTHDAY LIVE day2まで<br>" + days + "日 " + hours + "時間 "
   + minutes + "分 " + seconds + "秒 ";
 
   // If the count down is finished, write some text 
