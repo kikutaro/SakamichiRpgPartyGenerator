f94a8155bca66150972ecb41da48bfcdf70acd0d
kikutaro <kikutaro526@yahoo.co.jp>
2018-07-07 22:33:40 +0900

    乃木坂46バスラ3日目のカウントダウン開始

--- js/countdown.js
+++ js/countdown.js
@@ -9,12 +9,12 @@ var x = setInterval(function() {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  document.getElementById("countdown").innerHTML = "乃木坂46 6th YEAR BIRTHDAY LIVE day2まで<br>" + days + "日 " + hours + "時間 "
+  document.getElementById("countdown").innerHTML = "乃木坂46 6th YEAR BIRTHDAY LIVE day3まで<br>" + days + "日 " + hours + "時間 "
   + minutes + "分 " + seconds + "秒 ";
 
   // If the count down is finished, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("countdown").innerHTML = "開始！";
+    document.getElementById("countdown").innerHTML = "バスラ最終日開始！";
   }
 }, 1000);
