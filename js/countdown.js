7fa8b072d81f3f6eb6be9742dfea589246dff574
kikutaro <kikutaro526@yahoo.co.jp>
2018-07-21 08:59:37 +0900

    ライブ情報更新

--- js/countdown.js
+++ js/countdown.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("July 20, 2018 17:00:00").getTime();
+var countDownDate = new Date("July 21, 2018 17:00:00").getTime();
 
 var x = setInterval(function() {
 
@@ -9,12 +9,12 @@ var x = setInterval(function() {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  document.getElementById("countdown").innerHTML = "欅共和国2018まで<br>" + days + "日 " + hours + "時間 "
+  document.getElementById("countdown").innerHTML = "乃木坂46 真夏の全国ツアー2018 福岡ヤフオク!ドーム＆欅共和国2018 2日目<br>" + days + "日 " + hours + "時間 "
   + minutes + "分 " + seconds + "秒 ";
 
   // If the count down is finished, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("countdown").innerHTML = "欅共和国2018開始！";
+    document.getElementById("countdown").innerHTML = "乃木坂46 真夏の全国ツアー2018 福岡ヤフオク!ドーム＆欅共和国2018 2日目ライブ開始中！";
   }
 }, 1000);
